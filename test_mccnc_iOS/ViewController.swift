9283d4f44e31f2a104af49d1e4b52ff62864b496
Ivan <ivanbeat888@gmail.com>
2024-04-28 13:31:05 +0200

    Initial Commit

---
+++ test_mccnc_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_mccnc_iOS
+//
+//  Created by Иван Курганский on 28/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
